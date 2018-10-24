11c059d1cfb1945993de83bd6eff461caf193b6b
Yuumi <yuuming.n@gmail.com>
2018-10-24 14:14:52 -0700

    Delete app.js file

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
