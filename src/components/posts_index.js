11e633d641a8b436e008e4ef93766856201408dc
Yuumi <yuuming.n@gmail.com>
2018-10-25 23:38:24 -0700

    Create renderPosts function

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,3 +1,4 @@
+import _ from "lodash";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { fetchPosts } from "../actions";
@@ -6,9 +7,23 @@ class PostsIndex extends Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
+
+  renderPosts() {
+      return _.map(this.props.posts, post => {
+          return (
+              <li className="list-group-item" key={post.id}>
+                  {post.title}
+              </li>
+          );
+      });
+  }
   render() {
-    console.log(this.props.posts);
-    return <div>Post Index</div>;
+    return (
+      <div>
+        <h3>Posts</h3>
+        <ul className="list-group">{this.renderPosts()}</ul>
+      </div>
+    );
   }
 }
 function mapStateToProps(state) {
