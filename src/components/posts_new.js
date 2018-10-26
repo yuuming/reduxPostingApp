a5157552724b6633e0dd1b82ff1dca65b0d06357
Yuumi <yuuming.n@gmail.com>
2018-10-26 16:34:06 -0700

    Implement submit function

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -11,21 +11,37 @@ class PostsNew extends Component {
     );
   }
 
+  onSubmit(values) {
+      console.log(values);
+  }
+
   render() {
+      const { handleSubmit } = this.props;
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field label="Title" name="title" component={this.renderField} />
-        <Field label="Categories" name="Categories" component={this.renderField} />
+        <Field
+          label="Categories"
+          name="Categories"
+          component={this.renderField}
+        />
         <Field
           label="Post Content"
           name="content"
           component={this.renderField}
         />
+      <button type= "submit" className="btn btn-primaly">Submit</button>
+
       </form>
     );
   }
 }
 
+function validate(values) {
+
+}
+
 export default reduxForm({
+  validate,
   form: "PostsNewForm"
 })(PostsNew);
