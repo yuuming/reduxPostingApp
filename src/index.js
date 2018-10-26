40873b755163e02c8f2fe8c941656eeb4d944584
Yuumi <yuuming.n@gmail.com>
2018-10-25 23:56:39 -0700

    Add navigation between Index and New

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware } from "redux";
-import { BrowserRouter, Route } from "react-router-dom";
-import promise from 'redux-promise'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import promise from "redux-promise";
 
 import reducers from "./reducers";
 import PostsIndex from "./components/posts_index";
-import PostsNew from './components/posts_new';
+import PostsNew from "./components/posts_new";
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
@@ -15,8 +15,10 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path="/" component={PostsIndex} />
-        <Route path="/posts/new" component={PostsNew} />
+        <Switch>
+          <Route path="/posts/new" component={PostsNew} />
+          <Route path="/" component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>,
