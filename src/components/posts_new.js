3560d5dc96a8e1e07977e54a319e0615cae8ce94
Yuumi <yuuming.n@gmail.com>
2018-10-26 14:27:42 -0700

    Add input field in PostsNew

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,13 +1,31 @@
 import React, { Component } from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
+
+    renderTitleField(field) {
+        return (
+            <div>
+                <input
+                type="text"
+                {...field.input}
+                />
+            </div>
+        );
+    }
+
     render() {
         return(
-            <div>
-                PostNew!!!!
-            </div>
+            <form>
+                <Field
+                name="title"
+                component={this.renderTitleField}
+                />
+            </form>
         );
     }
 }
 
-export default PostsNew;
+export default reduxForm({
+    form: 'PostsNewForm'
+})(PostsNew);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 import PostReducer from './reducer_posts';
 
 const rootReducer = combineReducers({
