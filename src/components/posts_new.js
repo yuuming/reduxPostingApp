de104b627c982b25f709e1f14ce888cfac5805c7
Yuumi <yuuming.n@gmail.com>
2018-10-25 23:44:48 -0700

    Create posts_new.js

---
+++ src/components/posts_new.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class PostsNew extends Component {
+    render() {
+        return(
+            <div>
+                PostNew!!!!
+            </div>
+        );
+    }
+}
+
+export default PostsNew;
