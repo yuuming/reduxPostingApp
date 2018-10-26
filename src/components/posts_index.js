797c1012447c0b31dcba8641cf3889da5f2d8056
Yuumi <yuuming.n@gmail.com>
2018-10-25 21:00:26 -0700

    Fix a bug

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,23 +1,21 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchPosts } from "../actions";
 
 class PostsIndex extends Component {
-    componentDidMount() {
-            this.props.fetchPosts();
-        }
-    render () {
-        console.log(this.props.posts);
-        return (
-            <div>
-                Post Index
-            </div>
-        );
-    }
+  componentDidMount() {
+    this.props.fetchPosts();
+  }
+  render() {
+    console.log(this.props.posts);
+    return <div>Post Index</div>;
+  }
 }
 function mapStateToProps(state) {
-    return { posts: state.posts };
-} 
+  return { posts: state.posts };
+}
 
-
-export default connect(null, { fetchPosts })(PostsIndex); 
+export default connect(
+  mapStateToProps,
+  { fetchPosts }
+)(PostsIndex);
