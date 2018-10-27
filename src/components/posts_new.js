f83bd987313feebe2358002a74ec548751c49757
Yuumi <yuuming.n@gmail.com>
2018-10-27 16:57:39 -0700

    Implement validate functions

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -3,16 +3,16 @@ import { Field, reduxForm } from "redux-form";
 
 class PostsNew extends Component {
   renderField(field) {
-    const { meta: { touched, error }} = field;
-    const className = `form-group ${touched && error ? 'has-danger' : ''}`;
+    const {
+      meta: { touched, error }
+    } = field;
+    const className = `form-group ${touched && error ? "has-danger" : ""}`;
 
     return (
       <div className={className}>
-        <labal>{field.label}</labal>
+        <label>{field.label}</label>
         <input className="form-control" type="text" {...field.input} />
-        <div className="text-help">
-          {touched ? error : ""}
-        </div>
+        <div className="text-help">{touched ? error : ""}</div>
       </div>
     );
   }
@@ -28,7 +28,7 @@ class PostsNew extends Component {
         <Field label="Title" name="title" component={this.renderField} />
         <Field
           label="Categories"
-          name="Categories"
+          name="categories"
           component={this.renderField}
         />
         <Field
@@ -44,8 +44,21 @@ class PostsNew extends Component {
   }
 }
 
-function validate(values) {}
+function validate(values) {
+  const errors = {};
 
+  if (!values.title) {
+    errors.title = "Enter a title";
+  }
+  if (!values.categories) {
+    errors.categories = "Enter some categories";
+  }
+  if (!values.content) {
+    errors.content = "Enter some content please";
+  }
+
+  return errors;
+}
 export default reduxForm({
   validate,
   form: "PostsNewForm"
