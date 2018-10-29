e71d2947777aab44ed81ec51fb86da1e61913cf4
Yuumi <yuuming.n@gmail.com>
2018-10-28 19:54:45 -0700

    Implement render function show post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -4,21 +4,34 @@ import { fetchPost } from '../actions';
 
 class PostShow extends Component {
     componentDidMount() {
-     const { id } =  this.props.match.params.id;
+     const { id } =  this.props.match.params;
+     console.log('console this.props.match.params', this.props.match.params);
         this.props.fetchPost(id);
     }
 
     render() {
+
+        const { post } = this.props;
+
+        if(!post) {
+            return <div>Loading....</div>;
+        }
+
         return (
             <div>
-                Posts Show!
+                <h3>{post.title}</h3>
+                <h6>Categoires: {post.categories}</h6>
+                <p>{post.content}</p>
             </div>
         );
     }
 }
 
 function mapStateToProps({ posts }, ownProps) {
-    return { post: posts[ownProps.match.params.id]}
+    console.log('console ownProps', ownProps);
+
+    console.log('console posts[ownProps.match.params.id]', posts[ownProps.match.params.id]);
+    return { post: posts[ownProps.match.params.id]};
 }
 
 export default connect(mapStateToProps, { fetchPost })(PostShow);
