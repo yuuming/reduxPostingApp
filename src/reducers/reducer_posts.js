84a926cfa934ad850d356ef70483efbde1ac6f5d
Yuumi <yuuming.n@gmail.com>
2018-10-28 18:45:05 -0700

    Fix mistakes switch statement

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -4,15 +4,12 @@ import _ from 'lodash';
 export default function(state = {}, action) {
   switch (action.type) {
     case FETCH_POST:
-    return { ...state, };
-    case FETCH_POSTS:
-    // const post = action.payload.data;
+        // const post = action.payload.data;
     // const newState = { ...state };
     // newState[post.id] = post;
     // return newState;
     return { ...state, [action.payload.data.id]: action.payload.data };
-
-
+    case FETCH_POSTS:
       return _.mapKeys(action.payload.data, "id");
     default:
       return state;
