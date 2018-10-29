eeccfbc8a7f8d3989b2a96bd8f79e277e9ebb82a
Yuumi <yuuming.n@gmail.com>
2018-10-28 22:56:24 -0700

    Implement link tag for each post title

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { fetchPost } from '../actions';
 
 class PostShow extends Component {
@@ -10,15 +11,14 @@ class PostShow extends Component {
     }
 
     render() {
-
         const { post } = this.props;
-
         if(!post) {
             return <div>Loading....</div>;
         }
 
         return (
             <div>
+                <Link className="btn btn-primary" to="/" >Back to Index</Link>
                 <h3>{post.title}</h3>
                 <h6>Categoires: {post.categories}</h6>
                 <p>{post.content}</p>
