caf127c77a4b8da688a82fdd8c5f282c12a53cb5
Yuumi <yuuming.n@gmail.com>
2018-10-28 18:43:23 -0700

    Add switch statement FETCH_POST

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const FETCH_POSTS = "fetch_posts";
+export const FETCH_POST = "fetch_post";
 export const CREATE_POST = "create_post";
+
 const ROOT_URL = "http://reduxblog.herokuapp.com/api";
 const API_KEY = "?key=PAPERCLIP62000";
 
@@ -23,4 +25,13 @@ export function createPost(values, callback) {
     type: CREATE_POST,
     payload: request
   };
+}
+
+export function fetchPost(id) {
+  const request = axios.get(`${ROOT_URL}/posts/${id}${API_KEY}`);
+
+  return {
+    type: FETCH_POST,
+    payload: request
+  }
 }
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,9 +1,18 @@
-import { FETCH_POSTS } from "../actions";
+import { FETCH_POSTS, FETCH_POST } from "../actions";
 import _ from 'lodash';
 
 export default function(state = {}, action) {
   switch (action.type) {
+    case FETCH_POST:
+    return { ...state, };
     case FETCH_POSTS:
+    // const post = action.payload.data;
+    // const newState = { ...state };
+    // newState[post.id] = post;
+    // return newState;
+    return { ...state, [action.payload.data.id]: action.payload.data };
+
+
       return _.mapKeys(action.payload.data, "id");
     default:
       return state;
